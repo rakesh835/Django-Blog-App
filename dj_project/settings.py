8332e4a867fa620ee3cbdef37afb59134ca4b01f
rakeh835 <rakeshmandherna@gmail.com>
2022-03-08 19:58:40 +0530

    updated requirements.txt

--- dj_project/settings.py
+++ dj_project/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY=os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['rkawesomeblogs.herokuapp.app']
+ALLOWED_HOSTS = ['rkawesomeblogs.herokuapp.com']
 
 
 # Application definition
